948eebe7b61e574b760ae0ff64ee4221f34878a7
faridAbinah <faridabinah@hotmail.com>
2021-09-03 11:32:28 +0000

    Used a for loop to print numbers 100 to 200 with exceptions

---
+++ loopy-lighthouse.js
@@ -0,0 +1,21 @@
+let number = 100;
+
+for(number = 100; number <= 200; ++number)
+{
+  if(number % 3 === 0 && number % 4 === 0)
+  {
+    console.log(`LoopyLighthouse`);
+  }
+  else if(number % 4 === 0)
+  {
+    console.log(`Lighthouse`);
+  }
+  else if(number % 3 === 0)
+  {
+    console.log(`Loopy`);
+  }
+  else 
+  {
+    console.log(number);
+  }
+}
